52b4d8b4b2ad44d04c3c228281031c0051516750
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-10-05 19:22:39 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,42 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
 def is_anagram(first_string, second_string):
-    # Faça o código aqui.
+    if len(first_string) != len(second_string):
+        return False
+    first = merge_sort(list(first_string))
+    second = merge_sort(list(second_string))
+    answer = True
+    for item in range(len(first)):
+        if first[item] != second[item]:
+            answer = False
+    return answer
 
 
-first_string = ""
-second_string = ""
+first_string = "cassio"
+second_string = "aissoc"
 print(is_anagram(first_string, second_string))
