30ea9851b8a9a790f609bb3be5782689d7be84e1
douglas-he <doughrqe@gmail.com>
2020-10-05 15:38:17 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,9 @@
 def study_schedule(start_time, end_time, target_time):
-    # Faça o código aqui.
+    acumulador = 0
+    for index in range(len(start_time)):
+        if target_time >= start_time[index] and target_time <= end_time[index]:
+            acumulador += 1
+    return acumulador
 
 
 start_time = []
