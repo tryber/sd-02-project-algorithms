dbbb20ab50183ee31f07e117e61f9d791c812a43
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-10-05 15:49:53 -0300

    Iniciando projeto e requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    # Faça o código aqui.
+    counter = 0
+    for item in range(len(start_time)):
+        if start_time[item] <= target_time <= end_time[item]:
+            counter += 1
+    return counter
 
 
-start_time = []
-end_time = []
-target_time = 0
+start_time = [4, 1, 3, 2]
+end_time = [4, 3, 4, 5]
+target_time = 2
 print(study_schedule(start_time, end_time, target_time))
