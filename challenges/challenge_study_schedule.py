f45e8c4d52239a2f0b4df2ac609618e7ae797122
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-10-06 16:11:23 -0300

    Adicionando parâmetros ao requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
     counter = 0
+    if 0 > target_time > 23:
+        return counter
     for item in range(len(start_time)):
         if start_time[item] <= target_time <= end_time[item]:
             counter += 1
@@ -8,5 +10,5 @@ def study_schedule(start_time, end_time, target_time):
 
 start_time = [4, 1, 3, 2]
 end_time = [4, 3, 4, 5]
-target_time = 3
+target_time = 25
 print(study_schedule(start_time, end_time, target_time))
