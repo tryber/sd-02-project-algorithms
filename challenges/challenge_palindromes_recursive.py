45c975ff0356dcf93a3bb42c8fd8cf727850b256
Julio Cezar <jctaraujo@hotmail.com>
2020-10-05 22:42:35 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,10 @@
 def is_palindrome_recursive(word, low, high):
-    # Faça o código aqui.
+    if (word[low].lower() != word[high].lower()):
+        return False
+    else:
+        return True
+    return is_palindrome_recursive(word, low + 1, high - 1)
 
 
-word = ""
+word = "Socos"
 print(is_palindrome_recursive(word, 0, len(word) - 1))
