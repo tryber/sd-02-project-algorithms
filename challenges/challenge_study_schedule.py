ef1f2ff6c9b02de8dfbcd603cf270d3e955191de
phtf92 <phtf92@gmail.com>
2020-10-05 21:55:13 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    # Faça o código aqui.
+    count = 0
+    for index, start in enumerate(start_time):
+        if target_time >= start and target_time <= end_time[index]:
+            count += 1
+    return count
 
 
-start_time = []
-end_time = []
-target_time = 0
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+target_time = 1
 print(study_schedule(start_time, end_time, target_time))
