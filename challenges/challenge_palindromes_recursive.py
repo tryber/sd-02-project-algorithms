15833722e1d91674230d4f1f2489ce5a8742dcc1
guicgs <etohlif@protonmail.com>
2020-10-08 13:37:51 -0300

    requisitos 1 e 2 concluídos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,15 @@
 def is_palindrome_recursive(word, low, high):
-    # Faça o código aqui.
+    if not word:
+        return False
+
+    if high <= low:
+        return True
+
+    if word[high] != word[low]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low + 1, high - 1)
 
 
-word = ""
+word = "sopapos"
 print(is_palindrome_recursive(word, 0, len(word) - 1))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    # Faça o código aqui.
+    count = 0
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time <= end_time[index]:
+            count += 1
+    return count
 
 
-start_time = []
-end_time = []
-target_time = 0
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+target_time = 5
 print(study_schedule(start_time, end_time, target_time))
