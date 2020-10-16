cc5d9aace08d2726b0269fd3531e8e6423340034
eddyeboy27 <lipe0503@gmail.com>
2020-10-15 23:35:58 -0300

    Finalizado requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,17 @@
 def is_palindrome_recursive(word, low, high):
-    # Faça o código aqui.
+    if word == "":
+        return False
+    new_word = list(word)
+    new_word[low], new_word[high -
+                            low] = new_word[high - low], new_word[low]
+    if ''.join(new_word[::-1]) == word:
+        return True
+    else:
+        if word[low] == word[high] and low < high:
+            return is_palindrome_recursive(word, low + 1, high - 1)
+        else:
+            return False
 
 
-word = ""
+word = "agua"
 print(is_palindrome_recursive(word, 0, len(word) - 1))
