043373b82801ec69222b365f0015a8d69e9a1e93
eddyeboy27 <lipe0503@gmail.com>
2020-10-17 07:09:52 -0300

    Finalizado projeto

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,18 @@
 def is_anagram(first_string, second_string):
-    # Faça o código aqui.
+    if first_string[::-1] == second_string:
+        return True
+    word_st = list(first_string)
+    word_nd = list(second_string)
+    for i, letter in enumerate(word_st):
+        if letter != word_nd[i]:
+            index_letter = word_nd.index(letter)
+            word_st[i], word_st[index_letter] = word_st[index_letter], word_st[i]
+    if word_st == word_nd:
+        return True
+    else:
+        return False
 
 
-first_string = ""
-second_string = ""
+first_string = "futuro"
+second_string = "furtou"
 print(is_anagram(first_string, second_string))
