006b90a40eb78ab02a7b633c053f3d30362d05fe
eddyeboy27 <lipe0503@gmail.com>
2020-10-17 07:26:23 -0300

    criado função de busca do index

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,9 @@
+def how_index(lista, refer):
+    for index, item in enumerate(lista):
+        if item == refer:
+            return index
+
+
 def is_anagram(first_string, second_string):
     if first_string[::-1] == second_string:
         return True
@@ -5,7 +11,7 @@ def is_anagram(first_string, second_string):
     word_nd = list(second_string)
     for i, letter in enumerate(word_st):
         if letter != word_nd[i]:
-            index_letter = word_nd.index(letter)
+            index_letter = how_index(word_nd, letter)
             word_st[i], word_st[index_letter] = word_st[index_letter], word_st[i]
     if word_st == word_nd:
         return True
@@ -13,6 +19,6 @@ def is_anagram(first_string, second_string):
         return False
 
 
-first_string = "futuro"
-second_string = "furtou"
+first_string = "tfuuor"
+second_string = "futuro"
 print(is_anagram(first_string, second_string))
