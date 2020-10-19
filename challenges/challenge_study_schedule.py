f00ff3df686da4fa267668aa2e038f7e060e1b9e
Miguel Miranda <miguelmirandaazevedo@gmail.com>
2020-10-19 00:11:26 -0300

    requisito 2 + alterações no requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
     counter = 0
 
-    for index, _ in enumerate(start_time):
+    for index in range(len(start_time)):
         if start_time[index] <= target_time <= end_time[index]:
             counter += 1
 
@@ -11,4 +11,4 @@ def study_schedule(start_time, end_time, target_time):
 start_time = []
 end_time = []
 target_time = 0
-print(study_schedule([4, 1, 3, 2], [4, 3, 4, 5], 0))
+print(study_schedule(start_time, end_time, target_time))
