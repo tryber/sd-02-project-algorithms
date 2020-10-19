042efcd37cad3ce8c30feb9de8415eff8301c702
Miguel Miranda <miguelmirandaazevedo@gmail.com>
2020-10-19 01:30:56 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,25 @@
 def is_anagram(first_string, second_string):
-    pass
+    if len(first_string) != len(second_string):
+        return False
+
+    dictionary = dict()
+
+    for letter in first_string:
+        try:
+            dictionary[letter] += 1
+        except KeyError:
+            dictionary[letter] = 1
+
+    for letter in second_string:
+        try:
+            dictionary[letter] -= 1
+        except KeyError:
+            return False
+
+        if dictionary[letter] == -1:
+            return False
+
+    return True
 
 
 first_string = ""
