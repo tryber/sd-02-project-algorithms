ae988ea0595311707a27039003d642abcb93322a
Lauro Lyra Aguiar <laurolyra@gmail.com>
2020-10-23 18:54:31 -0300

    challenge 3 typo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,5 @@
 def is_anagram(first_string, second_string):
-    if(sorted(first_string) == sorted(second_string): 
+    if sorted(first_string) == sorted(second_string): 
         return True  
     else: 
         return False
