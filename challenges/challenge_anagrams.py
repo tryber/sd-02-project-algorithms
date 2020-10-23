4b0ba802baf479d8c5f179cda09bc91d37a0cb32
Lauro Lyra Aguiar <laurolyra@gmail.com>
2020-10-23 18:50:44 -0300

    Challenge 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,10 @@
 def is_anagram(first_string, second_string):
-    # Faça o código aqui.
+    if(sorted(first_string) == sorted(second_string): 
+        return True  
+    else: 
+        return False
 
-
+       
 first_string = ""
 second_string = ""
 print(is_anagram(first_string, second_string))
