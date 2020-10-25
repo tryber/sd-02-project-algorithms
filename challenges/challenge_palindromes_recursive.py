06f712bc26342ba9899d459b290d6dec3fe9f38b
Lauro Lyra Aguiar <lauro.aguiar@pagueveloz.com.br>
2020-10-25 12:38:19 -0300

    palindromes - empty string

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,4 +1,6 @@
 def is_palindrome_recursive(word, low, high):
+    if word == "":
+        return False
     if len(word) <= 1:
         return True
     if word[low] == word[high]:
@@ -8,5 +10,5 @@ def is_palindrome_recursive(word, low, high):
         return False
 
 
-word = "OLOBOAMAOBOLO"
+word = "ANA"
 print(is_palindrome_recursive(word, 0, len(word) - 1))
