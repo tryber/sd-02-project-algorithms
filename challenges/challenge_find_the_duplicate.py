7ed531fe34e252a89121e7ad83f0da538eca3c4e
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-09 12:45:04 -0300

    project

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,6 +1,25 @@
 def find_duplicate(nums):
-    # Faça o código aqui.
+    sorted_nums = sorted(nums)
+    for index, num in enumerate(sorted_nums):
+        if num == sorted_nums[index + 1]:
+            return num
+    return False
+
+nums = [3, 1, 2, 4, 6, 5, 7, 7, 7, 8]
+print(find_duplicate(nums))
 
 
-nums = []
+nums = [1, 3, 4, 2, 2]
+print(find_duplicate(nums))
+# saída: 2
+
+nums = [3, 1, 3, 4, 2]
+print(find_duplicate(nums))
+# saída: 3
+
+nums = [1, 1]
+print(find_duplicate(nums))
+# saída: 1
+
+nums = [1, 1, 2]
 print(find_duplicate(nums))
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,6 +1,25 @@
 def is_palindrome_iterative(word):
-    # Faça o código aqui.
+    if not word:
+        return False
+    middle = len(word) // 2
+    high = len(word) - 1
+    for index in range(0, middle):
+        if word[index] != word[high - index]:
+            return False
+    return True
 
+
+def is_phrase_palindrome(phrase):
+    string_phrase = "".join(phrase.split()).lower()
+    return is_palindrome_iterative(string_phrase)
 
 word = ""
 print(is_palindrome_iterative(word))
+
+word = "COXINHA"
+print(is_palindrome_iterative(word))
+word = "REVIVER"
+print(is_palindrome_iterative(word))
+phrase = "MELODRAMA IS THE ALBUM MUBLA EHT SI AMARDOLEM"
+print(is_phrase_palindrome(phrase))
+
